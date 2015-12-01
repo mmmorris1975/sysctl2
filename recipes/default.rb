0c4846f8fcecb35c030a16035e4bf5ac57d944b9
Mike Morris <michael.m.morris@pearson.com>
2015-12-01 16:17:48 -0600

    updated sysctl reload command with per-platform logic
    With the introduction of the /etc/sysctl.d directory to allow modularized
    configuration files, configuration reflected by files inside that directory
    were not being recognized when executing 'sysctl -p' (it only reloads files
    from /etc/sysctl.conf).  The new way to demand load sysctl configurations is
    to run the 'sysctl --system' command, which will load the config files from
    various locations.
    
    This cookbook was updated to use the new 'sysctl --system' command as the
    default sysctl command, while providing per-platform overrides to execute
    'sysctl -p' on systems where the --system flag isn't implemented.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,9 +7,19 @@
 
 conf_file = ::File.join(node['sysctl']['conf_dir'], node['sysctl']['conf_file'])
 
+sysctl_command = value_for_platform(
+  %w(centos redhat debian) => {
+    '< 7.0' => "sysctl -p #{conf_file}"
+  },
+  'ubuntu' => {
+    '< 14.0' => "sysctl -p #{conf_file}"
+  },
+  'default' => 'sysctl --system'
+)
+
 execute 'update kernel params' do
   action :nothing
-  command "sysctl -p #{conf_file}"
+  command sysctl_command
   returns [0, 255]
 end
 
